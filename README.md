9634df82cb896775badddb994930ec7cc11320d3
R <107578323+blue-slushy9@users.noreply.github.com>
2023-12-26 20:16:26 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# c-hello-world
+# Always wanted to learn C, so I am starting with Hello World! :) 
---
+++ hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // Preprocessor directive that tells the compiler to
+		   // include the standard input/output library; provides
+		   // functions like 'printf' and 'scanf';
+
+// Function prototype---declares to compiler that there is a function named
+// 'hello', and that it takes no parameters and returns no output, hence void;
+// in C, the compiler needs to know about a function's existence (i.e. its
+// prototype 
+void hello();
